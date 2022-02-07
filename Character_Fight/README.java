a6370ff9e920519e2aac96789a1ffc735eeea860
Joshua Solomon <jscottsolomon@gmail.com>
2022-02-07 16:22:19 -0700

    Update and rename README.java to README.txt

--- Character_Fight/README.java
+++
@@ -1,7 +0,0 @@
-/*
-*A text-based fight between two characters.
-*What I Learned:
-*  -Class naming conventions
-*  -Inheritance
-*  -Abstract classes
-*/
---
+++ Character_Fight/README.txt
@@ -0,0 +1,5 @@
+A text-based fight between two characters.
+What I Learned:
+  -Inheritance
+  -Abstract classes
+  -Class naming conventions
