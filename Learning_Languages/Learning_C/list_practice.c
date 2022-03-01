2ca8fd36e18a9c4068ec4ecf985afa443d92f53f
Joshua Solomon <jscottsolomon@gmail.com>
2022-03-01 16:52:09 -0700

    Update list_practice.c

--- Learning_Languages/Learning_C/list_practice.c
+++ Learning_Languages/Learning_C/list_practice.c
@@ -10,40 +10,6 @@ Points:
   (-1, 3)
 */
 
-/*basiclist.c (in Polynomials, C folder):
-#include <stdio.h>
-#include <stdlib.h>
-#include"basiclist.h"
-
-int list_add(node_t ** list, void * data) {
-  int ret = 0;
-  node_t * newnode = (node_t *) malloc(sizeof(node_t));
-  if (newnode == NULL) {
-    ret = -1;
-  }
-  else {
-    newnode->data = data;
-    newnode->next = *list;
-    *list = newnode;
-  }
-  return ret;
-}
-*/
-
-/*basiclist.h (in Polynomials, C folder):
-#ifndef BASICLIST_H
-#define BASICLIST_H
-
-typedef struct node {
-  void * data;        
-  struct node * next; 
-} node_t;
-
-int list_add(node_t ** list, void * data);
-#endif
-*/
-
-
 typedef struct {
   int x;
   int y;
@@ -77,3 +43,36 @@ int main(void) {
   }
   return 0;
 }
+
+/*basiclist.c (in Polynomials, C folder):
+#include <stdio.h>
+#include <stdlib.h>
+#include"basiclist.h"
+
+int list_add(node_t ** list, void * data) {
+  int ret = 0;
+  node_t * newnode = (node_t *) malloc(sizeof(node_t));
+  if (newnode == NULL) {
+    ret = -1;
+  }
+  else {
+    newnode->data = data;
+    newnode->next = *list;
+    *list = newnode;
+  }
+  return ret;
+}
+*/
+
+/*basiclist.h (in Polynomials, C folder):
+#ifndef BASICLIST_H
+#define BASICLIST_H
+
+typedef struct node {
+  void * data;        
+  struct node * next; 
+} node_t;
+
+int list_add(node_t ** list, void * data);
+#endif
+*/
