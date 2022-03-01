9a50ddff4cdbf0b1c7d7a627f9f721c23347bd49
Joshua Solomon <jscottsolomon@gmail.com>
2022-03-01 16:28:34 -0700

    Update Modulo.c

--- Learning C/Modulo.c
+++ Learning C/Modulo.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 
+/*Sample Output:
+1 2 three 4 five three 8 three five 11 three 13 fifteen 16 17
+three 19 five 22 23 three five 26 three 29 fifteen 31 32 three 34
+three 37 38 three five 41 43 44 fifteen 46 47 three
+*/
+
 int main(void){
     int count = 50;
 	int line_count = 0;
@@ -29,10 +35,5 @@ int main(void){
 		    }
        }
     return 0;
+	
 }
-
-/*Sample Output:
-1 2 three 4 five three 8 three five 11 three 13 fifteen 16 17
-three 19 five 22 23 three five 26 three 29 fifteen 31 32 three 34
-three 37 38 three five 41 43 44 fifteen 46 47 three
-*/
